4917bf3afa667c2b1a4f7829f45a03ca637ea9a3
Ankit raj <113342181+ankit-v2-3@users.noreply.github.com>
2024-10-25 16:23:11 +0530

    feat: add server docstring

--- backend/spielberg/entrypoint/api/socket_io.py
+++ backend/spielberg/entrypoint/api/socket_io.py
@@ -8,7 +8,10 @@ from spielberg.handler import ChatHandler
 
 
 class ChatNamespace(Namespace):
+    """Chat namespace for socket.io"""
+
     def on_chat(self, message):
+        """Handle chat messages"""
         chat_handler = ChatHandler(
             db=load_db(os.getenv("SERVER_DB_TYPE", app.config["DB_TYPE"]))
         )
